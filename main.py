b1ef8392ebd98ef1509cd6605fab4e0da157f757
MisustinIvan <ivanmis596@gmail.com>
2023-04-26 22:39:15 +0200

    made it oop, the scraper itself is a class that abstracts away all the stuff, can be used to build a discord bot now

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-def basicfunction():        #Function because using funtions = cool
-    print("Hello, World!")
+# MORTY, MORTY, I TURNED MYSELF INTO A PICKLE! PICKLE RICK!
+#import pickle
+import lunch_scraper
 
-basicfunction()
+
+def main():
+    site_url = "https://www.strava.cz/strava5/Jidelnicky?zarizeni=0253"
+    scraper = lunch_scraper.lunchScraper(site_url)
+
+    # some code that tests the cursed and evil pickle module, it is a joke
+
+        #pickled_scraper_file = open("./pickled_scraper", "wb")
+        #pickle.dump(scraper, pickled_scraper_file)
+        #pickled_scraper_file.close()
+
+        #pickled_scraper_file = open("./pickled_scraper", "rb")
+        #scraper = pickle.load(pickled_scraper_file)
+        #pickled_scraper_file.close()
+
+
+    print(scraper.lunches[0].date)
+    print(scraper.lunches[0].soup)
+    print(scraper.lunches[0].lunch)
+
+if __name__ == "__main__":
+    main()
